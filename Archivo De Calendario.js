03815df713e4197a2c506cfa207fae75677009d3
AleixSaPa <sapafamilia@gmail.com>
2023-09-20 17:03:37 +0200

    Re Crear El Codigo Del Index

--- Archivo De Calendario.js
+++ Archivo De Calendario.js
@@ -2,7 +2,7 @@ var Eventos =
     [
         {
             title: 'Cumpleanyos Yaya Lorenza',
-            start: '2023-08-01',
+            start: '2023-09-010',
             mes: 'Agosto',
             anyo: '2023'
         },
