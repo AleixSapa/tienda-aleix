f0a7d953f12bd5bf6c0111170491fc33d283a81c
AleixSaPa <sapafamilia@gmail.com>
2023-09-20 14:47:35 +0200

    Crear Los Archivos

---
+++ Archivo De Calendario.js
@@ -0,0 +1,9 @@
+var Eventos =
+    [
+        {
+            title: 'Cumpleanyos Yaya Lorenza',
+            start: '2023-08-01',
+            mes: 'Agosto',
+            anyo: '2023'
+        },
+    ]
