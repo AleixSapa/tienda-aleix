5e15591200f7d88d295bf3d03ae3e288c613a404
Esanpons <ecs200@hotmail.com>
2023-10-13 12:05:31 +0200

    Areglar Lo Del Calendario i poner el nombre bien a precio

--- ArchivoDeCalendario.js
+++ ArchivoDeCalendario.js
@@ -2,7 +2,7 @@ var Eventos =
     [
         {
             title: 'Cumpleanyos Yaya Lorenza',
-            start: '2023-09-010',
+            start: '2023-10-13',
             mes: 'Agosto',
             anyo: '2023'
         },
